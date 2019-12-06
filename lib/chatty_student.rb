961e79965d29eb21008e6a03a59e9fc83e9e779c
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2019-12-06 00:19:36 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -9,4 +9,10 @@ class ChattyStudent < Student
     What, you don't want any spoilers?
     Okay well let me just tell you who died..."
   end
+
+  def raise_hand
+    10.times do
+      super
+    end
+  end
 end
